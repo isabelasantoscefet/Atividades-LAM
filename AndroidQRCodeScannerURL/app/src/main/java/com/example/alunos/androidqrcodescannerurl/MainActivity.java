0844db61ae76a0ad009cd319602bc47930790db0
isabelasantoscefet <isabelasantoss001@gmail.com>
2018-04-16 11:26:46 -0300

    Aula 16/04

--- AndroidQRCodeScannerURL/app/src/main/java/com/example/alunos/androidqrcodescannerurl/MainActivity.java
+++ AndroidQRCodeScannerURL/app/src/main/java/com/example/alunos/androidqrcodescannerurl/MainActivity.java
@@ -8,22 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button btnScan;
     private TextView lblName, lblAddress;
     private String aUrl;
     private IntentIntegrator qrScan;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -36,34 +34,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View v){
         qrScan.initiateScan();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result == null) {
-            Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
-
-        } else{
-            try {
-                String string = result.getContents();
-                if ((string.indexOf("http://www") != 0) && (string.indexOf(".com") != 0)) {
-                    Intent intencao = new Intent(Intent.ACTION_VIEW, Uri.parse(string.toLowerCase()));
-
-                    startActivity(intencao);
-                } else {
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                try {
                     JSONObject obj = new JSONObject(result.getContents());
                     lblName.setText(obj.getString("name"));
                     lblAddress.setText(obj.getString("address"));
+                } catch (JSONException e) {
+                    String string = result.getContents();
+                    if ((string.indexOf("http://www") != 0) && (string.indexOf(".com") != 0)) {
+                        Intent intencao = new Intent(Intent.ACTION_VIEW, Uri.parse(string.toLowerCase()));
+                        startActivity(intencao);
+                    } else {
+                        e.printStackTrace();
+                        Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                    }
                 }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
             }
-
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
