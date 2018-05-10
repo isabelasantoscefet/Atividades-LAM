400382c7c5f1f5547deab4e8fa5b086993c4081a
isabelasantoscefet <isabelasantoss001@gmail.com>
2018-05-10 17:14:32 -0300

    Toolbar errado

--- Toolbar/app/src/androidTest/java/com/example/alunos/androidtoolbar/ExampleInstrumentedTest.java
+++ Toolbar/app/src/androidTest/java/com/example/alunos/androidtoolbar/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package br.wave.androidtoolbar;
+package com.example.alunos.androidtoolbar;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("br.wave.androidtoolbar", appContext.getPackageName());
+        assertEquals("com.example.alunos.androidtoolbar", appContext.getPackageName());
     }
 }
