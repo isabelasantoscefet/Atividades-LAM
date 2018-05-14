f74863fd4b4e20e07b2018c1e9d01e1608990eeb
isabelasantoscefet <isabelasantoss001@gmail.com>
2018-05-14 11:28:24 -0300

    Toolbar Finalizado

--- Toolbar/app/src/main/java/com/example/alunos/androidtoolbar/MainActivity.java
+++ Toolbar/app/src/main/java/com/example/alunos/androidtoolbar/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -46,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 new NavigationView.OnNavigationItemSelectedListener(){
                     @Override
                     public  boolean onNavigationItemSelected(MenuItem menuItem){
+                        Log.d("navItem","ok");
                         Fragment f = null;
                         int itemId = menuItem.getItemId();
 
